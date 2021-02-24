6185d4a6cdcbe3eabdf45013a05936ad26a6ab15
Nguyễn Cửu Quang Tuấn <quangtuan.0102@gmail.com>
2021-02-24 11:47:29 +0700

    push dang ky

--- fe/online-fashion-watch-shop/src/pages/login/login.jsx
+++ fe/online-fashion-watch-shop/src/pages/login/login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "./css/login.css";
+import Imgleft from "../../assets/image/img-login.jpg";
+import logoDw from "../../assets/image/dw-logo.jpg";
+import "boxicons";
 function Login() {
-  return <h1>Trang đăng ký</h1>;
+  return (
+    <div className="Login">
+      <div className="Login__left">
+        <div className="Login__img">
+            <img src={Imgleft} alt="Imgleft"/>
+        </div>
+      </div>
+      <div className="Login__left">
+        <a href="/" className="Login__logo">
+        <img src={logoDw} alt="" />
+        </a>
+        <div className="Login__form">
+          <form className="form__login">
+            <div className="form__control">
+            <box-icon name="mail-send"></box-icon>
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="form__control">
+            <box-icon name='lock-alt' type='solid' ></box-icon>
+              <input type="Password" placeholder="Password" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default Login;
